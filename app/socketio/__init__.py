5f191f44c97c8648c9f9c34a379afd303a02204f
Moacir <guedesmoacir12@gmail.com>
2025-04-02 09:53:32 -0300

    websocket fix

--- app/socketio/__init__.py
+++ app/socketio/__init__.py
@@ -1,6 +1,6 @@
 import socketio
 
-sio = socketio.AsyncServer(cors_allowed_origins="*")
-socketio_app = socketio.ASGIApp(sio)
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
+socketio_app = socketio.ASGIApp(sio, socketio_path="socket")
 
 __all__ = ["sio", "socketio_app"]
